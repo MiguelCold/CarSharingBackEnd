624f5778df4cea57c0b71a9f758664422530557c
Miguel Ángel Ossa Ruiz <miguelcold8@gmail.com>
2015-04-25 11:26:57 -0500

    Adding insert method to implementation of Credit Card DAO.

--- CarSharingBackEnd/src/co/edu/udea/carsharing/persistence/dao/impl/CreditCardDAOImpl.java
+++ CarSharingBackEnd/src/co/edu/udea/carsharing/persistence/dao/impl/CreditCardDAOImpl.java
@@ -1,11 +1,32 @@
 package co.edu.udea.carsharing.persistence.dao.impl;
 
+import java.net.UnknownHostException;
 import java.util.List;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.WriteResult;
+
 import co.edu.udea.carsharing.model.entities.CreditCard;
+import co.edu.udea.carsharing.persistence.conection.MongoDBConnector;
 import co.edu.udea.carsharing.persistence.dao.ICreditCardDAO;
 
 public class CreditCardDAOImpl implements ICreditCardDAO {
+
+	private static ICreditCardDAO instance;
+	private final DBCollection collection;
+
+	private CreditCardDAOImpl() throws UnknownHostException {
+		this.collection = MongoDBConnector.connect();
+	}
+
+	public static synchronized ICreditCardDAO getInstance()
+			throws UnknownHostException {
+		if (instance == null) {
+			instance = new CreditCardDAOImpl();
+		}
+		return (instance);
+	}
 
 	@Override
 	public CreditCard find(String type, String number) {
@@ -21,8 +42,11 @@ public class CreditCardDAOImpl implements ICreditCardDAO {
 
 	@Override
 	public boolean insert(CreditCard creditCard) {
-		// TODO Auto-generated method stub
-		return false;
+		BasicDBObject basicDBObject = (BasicDBObject) creditCard
+				.entityToDBObject();
+		WriteResult wr = this.collection.insert(basicDBObject);
+
+		return (wr.getN() == 1);
 	}
 
 	@Override
