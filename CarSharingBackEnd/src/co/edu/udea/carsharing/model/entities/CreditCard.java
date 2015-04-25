1483489962ba92d25982858ec1d35daa12b566a8
Miguel Ángel Ossa Ruiz <miguelcold8@gmail.com>
2015-04-25 10:34:56 -0500

    Adding the credit card to persistence layer in mongoDB

--- CarSharingBackEnd/src/co/edu/udea/carsharing/model/entities/CreditCard.java
+++ CarSharingBackEnd/src/co/edu/udea/carsharing/model/entities/CreditCard.java
@@ -2,9 +2,23 @@ package co.edu.udea.carsharing.model.entities;
 
 import java.io.Serializable;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+/**
+*
+* @author Andersson Garc&iacute;a Sotelo
+* @author Miguel &Aacute;ngel Ossa Ruiz
+* @author Juan Pablo Nore&ntilde;a
+* @author Juan Camilo Chaverra
+* 
+*/
 public class CreditCard implements Serializable {
 
 	private static final long serialVersionUID = -7706807582765491066L;
+
+	private static String TYPE = "type";
+	private static String NUMBER = "number";
 
 	private String type;
 	private String number;
@@ -16,6 +30,22 @@ public class CreditCard implements Serializable {
 	public CreditCard(String type, String number) {
 		this.type = type;
 		this.number = number;
+	}
+
+	public static CreditCard entityFromDBObject(DBObject dbObject) {
+		CreditCard creditCard = new CreditCard();
+		if (dbObject.containsField("TYPE") && dbObject.containsField("NUMBER")) {
+			creditCard.setType((String) dbObject.get(TYPE));
+			creditCard.setNumber((String) dbObject.get(NUMBER));
+		}
+		return (creditCard);
+	}
+
+	public DBObject entityToDBObject() {
+		BasicDBObject basicDBObject = new BasicDBObject();
+		basicDBObject.put(TYPE, this.getType());
+		basicDBObject.put(NUMBER, this.getNumber());
+		return (basicDBObject);
 	}
 
 	public String getType() {
