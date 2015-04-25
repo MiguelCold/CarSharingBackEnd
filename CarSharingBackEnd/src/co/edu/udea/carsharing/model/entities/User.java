eed21292e432d6e9e945871123bb58fa9422076b
Andersson Garcia Sotelo <anderssong5@outlook.com>
2015-04-25 10:56:59 -0500

    Agregando métodos para mapear con MongoDB

--- CarSharingBackEnd/src/co/edu/udea/carsharing/model/entities/User.java
+++ CarSharingBackEnd/src/co/edu/udea/carsharing/model/entities/User.java
@@ -1,19 +1,32 @@
 package co.edu.udea.carsharing.model.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 8155172105840619244L;
+
+	private static String NAME = "name";
+	private static String LAST_NAME = "lastName";
+	private static String EMAIL = "email";
+	private static String PASSWORD = "password";
+	private static String FACEBOOK_ID = "facebookId";
+	private static String CREDIT_CARDS = "creditCards";
+	private static String BIRTHDATE = "birthDate";
 
 	private String name;
 	private String lastName;
 	private String email;
 	private String password;
 	private String facebookId;
-	private List<CreditCard> creditcards;
+	private List<CreditCard> creditCards;
 	private Date birthDate;
 
 	public User() {
@@ -38,8 +51,87 @@ public class User implements Serializable {
 		this.email = email;
 		this.password = password;
 		this.facebookId = facebookId;
-		this.creditcards = creditcards;
+		this.creditCards = creditcards;
 		this.birthDate = birthDate;
+	}
+
+	public static User entityFromDBObject(DBObject dbObject) {
+		User user = new User();
+
+		if (dbObject.containsField(NAME)) {
+			user.setName((String) dbObject.get(NAME));
+		}
+
+		if (dbObject.containsField(LAST_NAME)) {
+			user.setLastName((String) dbObject.get(LAST_NAME));
+		}
+
+		if (dbObject.containsField(EMAIL)) {
+			user.setEmail((String) dbObject.get(EMAIL));
+		}
+
+		if (dbObject.containsField(PASSWORD)) {
+			user.setPassword((String) dbObject.get(PASSWORD));
+		}
+
+		if (dbObject.containsField(FACEBOOK_ID)) {
+			user.setFacebookId((String) dbObject.get(FACEBOOK_ID));
+		}
+
+		if (dbObject.containsField(BIRTHDATE)) {
+			user.setBirthDate((Date) dbObject.get(BIRTHDATE));
+		}
+
+		if (dbObject.containsField(CREDIT_CARDS)) {
+			BasicDBList basicDBbList = (BasicDBList) dbObject.get(CREDIT_CARDS);
+			user.setCreditCards(new ArrayList<CreditCard>());
+			for (Object object : basicDBbList) {
+				user.getCreditCards().add(
+						CreditCard.entityFromDBObject((BasicDBObject) object));
+			}
+		}
+
+		return (user);
+	}
+
+	public BasicDBObject entityToDBObject() {
+		BasicDBObject basicDBObject = new BasicDBObject();
+
+		if (null != this.getBirthDate()) {
+			basicDBObject.put(BIRTHDATE, this.getBirthDate());
+		}
+
+		if (null != this.getEmail()) {
+			basicDBObject.put(EMAIL, this.getEmail());
+		}
+
+		if (null != this.getFacebookId()) {
+			basicDBObject.put(FACEBOOK_ID, this.getFacebookId());
+		}
+
+		if (null != this.getLastName()) {
+			basicDBObject.put(LAST_NAME, this.getLastName());
+		}
+
+		if (null != this.getPassword()) {
+			basicDBObject.put(PASSWORD, this.getPassword());
+		}
+
+		if (null != this.getName()) {
+			basicDBObject.put(NAME, this.getName());
+		}
+
+		if (null != this.getCreditCards()) {
+			BasicDBList basicDBList = new BasicDBList();
+
+			for (CreditCard creditCard : this.getCreditCards()) {
+				basicDBList.add(creditCard.entityToDBObject());
+			}
+
+			basicDBObject.put(CREDIT_CARDS, basicDBList);
+		}
+
+		return (basicDBObject);
 	}
 
 	public String getName() {
@@ -82,12 +174,12 @@ public class User implements Serializable {
 		this.facebookId = facebookId;
 	}
 
-	public List<CreditCard> getCreditcards() {
-		return creditcards;
+	public List<CreditCard> getCreditCards() {
+		return creditCards;
 	}
 
-	public void setCreditcards(List<CreditCard> creditcards) {
-		this.creditcards = creditcards;
+	public void setCreditCards(List<CreditCard> creditCards) {
+		this.creditCards = creditCards;
 	}
 
 	public Date getBirthDate() {
