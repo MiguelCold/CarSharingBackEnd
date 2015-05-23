4809a6d574b72a46c9d5e0bc9f41443fb9d98b7d
Andersson García Sotelo <anderssongarciasotelo@gmail.com>
2015-05-23 15:46:52 -0500

    Mejorando implementación de servicio para Brand
    * Se mejor implementación del servicio relacionado con Brand: se cambia
    la forma de llamar la capa del negocio y la forma de validar.

--- CarSharingBackEnd/src/co/edu/udea/carsharing/ws/rest/impl/BrandWSImpl.java
+++ CarSharingBackEnd/src/co/edu/udea/carsharing/ws/rest/impl/BrandWSImpl.java
@@ -12,30 +12,24 @@ import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import co.edu.udea.carsharing.business.IBrandBusiness;
 import co.edu.udea.carsharing.business.exception.CarSharingBusinessException;
 import co.edu.udea.carsharing.business.impl.BrandBusinessImpl;
 import co.edu.udea.carsharing.model.entities.Brand;
 import co.edu.udea.carsharing.technical.exception.CarSharingTechnicalException;
 import co.edu.udea.carsharing.ws.IBrandWS;
+import co.edu.udea.carsharing.ws.exception.CarSharingWSException;
 import co.edu.udea.carsharing.ws.rest.contract.RESTFulWebServicesContract;
+import co.edu.udea.carsharing.ws.rest.util.WSUtil;
 
 @Path(value = RESTFulWebServicesContract.BrandtWebServicesContract.ROOT_PATH)
 public class BrandWSImpl implements IBrandWS {
-
-	private IBrandBusiness brandBusiness;
-
-	public BrandWSImpl() {
-		this.brandBusiness = BrandBusinessImpl.getInstance();
-	}
 
 	@POST()
 	@Consumes(value = { MediaType.APPLICATION_JSON })
 	@Produces(value = { MediaType.APPLICATION_JSON })
 	@Override()
-	public Response insert(Brand brand) {
-		if (brand == null || brand.getBrand() == null
-				|| brand.getBrand().trim().isEmpty()) {
+	public Response insert(Brand brand) throws CarSharingWSException {
+		if (!WSUtil.validateBrand(brand)) {
 
 			return Response.status(Response.Status.BAD_REQUEST).build();
 		}
@@ -43,7 +37,7 @@ public class BrandWSImpl implements IBrandWS {
 		Brand b;
 		try {
 
-			b = this.brandBusiness.insert(brand);
+			b = BrandBusinessImpl.getInstance().insert(brand);
 		} catch (CarSharingBusinessException e) {
 
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
@@ -57,11 +51,11 @@ public class BrandWSImpl implements IBrandWS {
 	@GET()
 	@Produces(value = { MediaType.APPLICATION_JSON })
 	@Override()
-	public Response findAll() {
+	public Response findAll() throws CarSharingWSException {
 		List<Brand> brands = new ArrayList<Brand>();
 
 		try {
-			brands = this.brandBusiness.findAll();
+			brands = BrandBusinessImpl.getInstance().findAll();
 		} catch (CarSharingBusinessException | CarSharingTechnicalException e) {
 
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
